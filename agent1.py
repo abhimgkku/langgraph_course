27d28861bc6713f0e7900f4a94e0d400963afff3
abhinav kumar <abhinav.mg.k@gmail.com>
2025-06-29 21:07:18 +0530

    updates done

---
+++ agent1.py
@@ -0,0 +1,9 @@
+from langgraph.graph import StateGraph, END
+import random
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from typing import Dict, List, TypedDict
+load_dotenv()
+
+print("sab kuch sahi chal raha hai")
